96f6d698a1afa2dd40726b36a694d415d8abf112
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-15 16:28:11 -0400

    create a new branch for database and write schema

---
+++ db/migrations/01_schema.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS menus CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS order_details CASCADE;
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  phone INTEGER,
+  email VARCHAR(255),
+  password VARCHAR(255),
+  address VARCHAR(255)
+);
+
+CREATE TABLE menus (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  price NUMERIC,
+  image_url VARCHAR(255),
+  ingredients VARCHAR(255),
+  status BOOLEAN
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  order_time TIMESTAMP,
+  status BOOLEAN,
+  price NUMERIC
+);
+
+CREATE TABLE order_details (
+  id SERIAL PRIMARY KEY,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_id INTEGER REFERENCES menus(id) ON DELETE CASCADE,
+  quantity SMALLINT
+);
---
+++ db/seeds/1_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (name, phone, email, password, address)
+VALUES ('zsh', 123, 'zsh@gmail.com','zsh','home'),
+        ('ujay', 321, 'ujay@gmail.com','ujay','home'),
+        ('ebuka', 132, 'ebuka@gmail.com','ebuka','home')
