65d0fafad9867ad85fc3f9f41b0d92c9c0d3d306
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-15 16:31:29 -0400

    insert dummu values to the table

--- db/seeds/1_seeds.sql
+++ db/seeds/1_seeds.sql
@@ -1,4 +1,6 @@
 INSERT INTO customers (name, phone, email, password, address)
 VALUES ('zsh', 123, 'zsh@gmail.com','zsh','home'),
         ('ujay', 321, 'ujay@gmail.com','ujay','home'),
-        ('ebuka', 132, 'ebuka@gmail.com','ebuka','home')
+        ('ebuka', 132, 'ebuka@gmail.com','ebuka','home');
+
+INSERT INTO
