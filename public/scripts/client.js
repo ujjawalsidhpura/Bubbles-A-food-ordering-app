28a00b3aa97bebe40003abeb09da69ae188790db
EbukaMoneme <camoneme@gmail.com>
2021-10-17 01:17:14 +0000

    Implement login function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,65 +1,69 @@
 $(() => {
+  loadMenus();
 
-  const createMenuItem = (menuItem) => {
-    const $menuItem = `
-    <div class="card">
-      <div class="card-image">
-        <img src=${menuItem.image_url} alt="Placeholder image">
+  $('#submit').on('submit', function(event) {
+    event.preventDefault();
+    console.log(this)
+    const serializedData = $(this).serialize();
+    console.log(serializedData)
+    $.post('/api/login', serializedData)
+
+  })
+
+})
+
+const createMenuItem = (menuItem) => {
+  const $menuItem = `
+  <div class="card">
+    <div class="card-image">
+      <img src=${menuItem.image_url} alt="Placeholder image">
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <p class="title is-3">${menuItem.name}</p>
       </div>
-      <div class="card-content">
-        <div class="media">
-          <p class="title is-3">${menuItem.name}</p>
-        </div>
-        <div class="content">
-          <span class="description"><b>Description: </b>${menuItem.description} </span>
-          <br>
-          <span class="ingredients"><b>Ingredients: </b>${menuItem.ingredients} </span>
-          <div class="card-footer">
-            <div class="cost">
-              <p><b>Price: </b>${menuItem.price}</p>
-            </div>
-            <div class="order-value">
-              <div>quantity: 0</div>
-              <div class="increment-order">
-                <button>+</button>
-                <button>-</button>
-              </div>
+      <div class="content">
+        <span class="description"><b>Description: </b>${menuItem.description} </span>
+        <br>
+        <span class="ingredients"><b>Ingredients: </b>${menuItem.ingredients} </span>
+        <div class="card-footer">
+          <div class="cost">
+            <p><b>Price: </b>${menuItem.price}</p>
+          </div>
+          <div class="order-value">
+            <div>quantity: 0</div>
+            <div class="increment-order">
+              <button class="button is-success is-light">+</button>
+              <button class="button is-danger is-light">-</button>
             </div>
           </div>
         </div>
       </div>
     </div>
-    `
-    return $menuItem;
-  };
+  </div>
+  `
+  return $menuItem;
+};
 
-  const renderMenus = function(menus_data) {
-    for (const menuItem of menus_data) {
-      const $menuItem = createMenuItem(menuItem);
-      $(".menu-container").append($menuItem);
-    }
+const renderMenus = function(menus_data) {
+  for (const menuItem of menus_data) {
+    const $menuItem = createMenuItem(menuItem);
+    $(".menu-container").append($menuItem);
   }
+}
 
-  //renderMenus(menus);
+const loadMenus = () => {
+  $.ajax({
+    url: '/api/menus',
+    method: "GET",
+    dataType: "json",
+    success: (menus) => {
+      renderMenus(menus);
+    },
 
-  const loadMenus = () => {
-    $.ajax({
-      url: '/api/menus',
-      method: "GET",
-      dataType: "json",
-      success: (menus) => {
-        renderMenus(menus);
-      },
-
-      error: (err) => {
-        alert(`there was an error ${err}`);
-      }
-    });
-  };
-
-  loadMenus();
-
-})
-
-
+    error: (err) => {
+      alert(`there was an error ${err}`);
+    }
+  });
+};
 
--- server/server.js
+++ server/server.js
@@ -36,13 +36,14 @@ app.use(express.static("public"));
 const usersRoutes = require("../routes/users");
 const widgetsRoutes = require("../routes/widgets");
 const menusRoutes = require("../routes/menus");
+const loginRoutes = require("../routes/login");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/menus", menusRoutes(db));
+app.use("/api/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
 
 // Home page
 // Warning: avoid creating more routes in this file!
