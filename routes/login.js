2a06e3c93f77ee7a3606f17797dfd67a4ccaa7ea
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-17 11:18:11 -0400

    fix cookie issue

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
@@ -14,15 +14,28 @@ const salt = bcrypt.genSaltSync(10);
 router.use(cookieSession({
   name: 'session',
   keys: ['This is a key that Im using to encrypt', '$!2@as125AF42%^&*'],
+  maxAge: 24 * 60 * 60 * 1000
 }))
 
 
 
 module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM customers;`)
+      .then(data => {
+        const cutomers = data.rows;
+        res.json(cutomers);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   router.post("/", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(req.session.user_id)
     console.log(email)
     console.log(password)
     db.query(`SELECT * FROM customers;`)
@@ -31,8 +44,11 @@ module.exports = (db) => {
         console.log(users)
         const user = login(users, email, password)
         if (user) {
-          console.log(user.id)
           req.session.user_id = user.id;
+          console.log(req.session.user_id)
+          res.send("Account Created !!!");
+        } else {
+          console.log("wrong password and username");
         }
       })
       .catch(err => {
@@ -41,6 +57,7 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
   return router;
 };
 
@@ -49,7 +66,12 @@ const login = (users, email, password) => {
   console.log(user)
 
   if (!user) {
-    console.log("no user")
+    console.log("no user");
+    return;
+  }
+
+  if (user.password !== password){
+    console.log("wrong password");
     return;
   }
   // if (!bcrypt.compareSync(password, user.password)) {
