9781307b9e0d95e8a8790c45a0ea334ea0d5d028
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-18 16:46:44 +0000

    sms.js moved into /public/script

--- public/scripts/sms.js
+++ public/scripts/sms.js
@@ -1,32 +1,29 @@
-// Import Twilio auth credentials from .env  //
-require('dotenv').config({ path: '../../.env' });
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const sms = require('twilio')(accountSid, authToken);
+// $(() => {
 
-/* Code Below in the box is temp and will change later when input routes and admin route to decide time for an order is establishes **/
+//   $('#order-submit-btn').on('click', function () {
+//     sendSMS();
+//   })
 
-const testObject = {
-  user_name: 'John Doe',
-  user_number: '+12048089972',
-  order_item: 'matcha tea'
-}
+// })
 
+const sendSMS = function () {
+  // Import Twilio auth credentials from .env  //
+  require('dotenv').config({ path: '../../.env' });
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const sms = require('twilio')(accountSid, authToken);
 
-
-const sendSMS = (obj) => {
-
-  const bubbleTeaStoreNum = '+13433125653'; // Do not alter //
 
   sms.messages
     .create({
-      body: `Thank you ${obj.user_name} for placing an order.Your item ${obj.order_item} will be ready in X minutes`,
-      from: bubbleTeaStoreNum,
-      to: obj.user_number
+      body: `An order has been placed from ABC. Order is ABC.`,
+      from: '+13433125653', // Twilio num for Restaurant
+      to: '+12048089972'
     })
     .then(message => console.log('Message ID', message.sid));
 }
 
-sendSMS(testObject);
+sendSMS();
+
 
