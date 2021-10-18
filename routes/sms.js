b249719449421acddc98d805bdd9b6c100110981
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-18 15:26:21 +0000

    refactoring sms.js

--- routes/sms.js
+++ routes/sms.js
@@ -1,18 +1,19 @@
-// Import Twilio auth credentials from .env in root dir //
+// Import Twilio auth credentials from .env  //
 require('dotenv').config({ path: '../.env' });
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-// Initilize client (OR sms) //
 const sms = require('twilio')(accountSid, authToken);
 
-/**  Code Below for 'testObject' will be changed later, once order input object is established **/
+/* Code Below in the box is temp and will change later when input routes and admin route to decide time for an order is establishes **/
 
 const testObject = {
-  user_name: 'vlad',
-  user_number: '+16478302809',
+  user_name: 'John Doe',
+  user_number: '+12048089972',
   order_item: 'matcha tea'
 }
+
+/***********************************************************/
 
 const sendSMS = (obj) => {
 
@@ -21,7 +22,6 @@ const sendSMS = (obj) => {
   sms.messages
     .create({
       body: `Thank you ${obj.user_name} for placing an order.Your item ${obj.order_item} will be ready in X minutes`,
-      //
       from: bubbleTeaStoreNum,
       to: obj.user_number
     })
