bd186a9046a05d845d5c1b776a243d5e68bc6e49
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-20 21:47:52 +0000

    test

--- public/scripts/smsClient.js
+++ public/scripts/smsClient.js
@@ -4,7 +4,7 @@ $(() => {
   $('#order-submit-btn').on('click', function (event) {
     event.preventDefault();
 
-    //INSERT ORDER and customer DETAILS into DB AND THEN-->
+    // ORDER and customer DET//INSERT AILS into DB AND THEN-->
 
 
     $.post('/api/orders', { order_id: 3 })
