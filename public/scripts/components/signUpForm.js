1ed1be1a70a4e5ba75a9fccfa0713e73a33ff64a
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-21 13:22:28 +0000

    value of +1 added to add phone number input

--- public/scripts/components/signUpForm.js
+++ public/scripts/components/signUpForm.js
@@ -2,7 +2,7 @@ $(() => {
   const signUpForm = $(`
   <form action="/users" method="POST" id="register">
     <input name="name" class="input" type="text" id="name" placeholder="First Name" required>
-    <input name="phone" class="input" type="tel" id="phone" placeholder="Phone Number" required>
+    <input name="phone" class="input" type="tel" id="phone" placeholder="Phone Number" value="+1" required>
     <input name="email" class="input" type="email" id="reg-email" placeholder="Email Address" required>
     <input name="password" class="input" type="password" id="reg-password" placeholder="Password" required>
     <input name="address" class="input" type="text" id="address" placeholder="Address" required>
